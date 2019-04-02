5691a737a59601302e8c9c6d6946fefa634365f0
vishav <vishavmahajan@gmail.com>
2019-04-02 16:02:18 +0530

    front app urls & views file modified vishu

--- front_app/urls.py
+++ front_app/urls.py
@@ -9,15 +9,15 @@ urlpatterns = [
     url(r'^error$',views.page_not_found,name="error"),
     url(r'^about/$',views.about,name="about"),
     url(r'^contact$',views.contact,name="contact"),
-    url(r'^driver$',views.driver_page,name="driver"),
+
     url(r'^faq$',views.faq,name="faq"),
     url(r'^package$',views.package,name="package"),
-    url(r'^register$',views.register,name="register"),
+
     url(r'^gallery$',views.gallery,name="gallery"),
     url(r'^team$',views.help,name="team"),
     url(r'^login$', views.login, name="login"),
-url(r'^admin$', views.admin_login, name="admin_login"),
+
     url(r'^signup$', views.signup, name="signup"),
-    url(r'^master/$', views.master, name="master"),
+    url(r'^masteruser/$', views.master, name="master"),
 url(r'^car-details/$', views.car_detail, name="baleno"),
 ]
--- front_app/views.py
+++ front_app/views.py
@@ -6,55 +6,43 @@ def index(request):
 
     return  render(request, "index.html")
 
-def login(request) :
-    if ( request.method == "POST" ):
+def login(request):
+    if(request.method == "POST"):
         useremail = request.POST['user_email']
         userpassword = request.POST['user_password']
-
         try:
             userdata = MySiteUser.objects.get(user_email=useremail)
-
             dp = userdata.user_password
-
+            get_id = userdata.site_role_id_id
             if (dp == userpassword):
-                # return render(request,"login1.html")
-                return redirect("/about/")
-
+                if get_id==1:
+                    return render(request, "about.html")
+                if get_id==4:
+                    return render(request, "contact.html")
+                if get_id==3:
+                    return render(request, "help-desk.html")
+            else:
+                return render(request, "404.html")
         except:
             return render(request,"404.html")
     return render(request,"login.html")
-def admin_login(request) :
-    if ( request.method == "POST" ):
-        useremail = request.POST['user_email']
-        userpassword = request.POST['user_password']
 
-        try:
-            userdata = MySiteUser.objects.get(user_email=useremail)
-
-            dp = userdata.user_password
-
-            if (dp == userpassword):
-                # return render(request,"login1.html")
-                return redirect("/about/")
-
-        except:
-            return render(request,"404.html")
-    return render(request,"admin_login.html")
 
 def signup(request):
     if (request.method == "POST"):
             form = MySiteUserForm(request.POST)
-            f1 = form.save(commit=False)
-            f1.user_name = request.POST['user_name']
-            f1.user_email = request.POST['user_email']
-            f1.user_mobile = request.POST['user_mobile']
-            f1.user_password = request.POST['user_password']
-            f1.site_role_id_id=1
-            f1.save()
-
-            return render(request, "login.html", {'inserted': True})
-
-    return render(request, "signup.html")
+            if form.is_valid():
+                f1 = form.save(commit=False)
+                f1.user_name = request.POST['user_name']
+                f1.user_email = request.POST['user_email']
+                f1.user_mobile = request.POST['user_mobile']
+                f1.user_password = request.POST['user_password']
+                f1.site_role_id_id=request.POST['sel']
+                f1.save()
+                return render(request, "login.html", {'valid': True})
+            else:
+                return render(request, "register.html", {'invalid': True})
+    return render(request, "register.html")
 
 
 
@@ -68,8 +56,6 @@ def about(request):
 def contact(request):
     return render(request,"contact.html")
 
-def driver_page(request):
-    return render(request,"driver.html")
 
 def faq(request):
     return render(request,"faq.html")
